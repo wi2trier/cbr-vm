880bb7bbef1a5b620f1aee308f50faea11e24484
Mirko Lenz <mirko@mirkolenz.com>
2024-02-21 10:00:39 +0100

    fix: remove non-functional virtualbox fix

--- profiles/virtualbox.nix
+++ profiles/virtualbox.nix
@@ -1,10 +1,4 @@
-{
-  modulesPath,
-  config,
-  lib,
-  pkgs,
-  ...
-}:
+{ modulesPath, ... }:
 {
   imports = [
     (modulesPath + "/virtualisation/virtualbox-image.nix")
@@ -12,21 +6,4 @@
   ];
 
   services.xserver.videoDrivers = [ "virtualbox" ];
-
-  # https://github.com/NixOS/nixpkgs/issues/243671#issuecomment-1822983422
-  services.xserver.displayManager.sessionCommands =
-    let
-      inherit (config.boot.kernelPackages) virtualboxGuestAdditions;
-      path = lib.makeBinPath (
-        with pkgs;
-        [
-          gnugrep
-          xorg.xorgserver.out
-          which
-        ]
-      );
-    in
-    ''
-      PATH="${path}:$PATH"  ${lib.getExe' virtualboxGuestAdditions "VBoxClient"} --vmsvga
-    '';
 }
