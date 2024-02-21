d6c4307f898f81b12fabbe7ed0ba71fa79cb7bbe
Mirko Lenz <mirko@mirkolenz.com>
2024-02-21 09:49:24 +0100

    fix: add virtualbox fix for window resizing

--- profiles/virtualbox.nix
+++ profiles/virtualbox.nix
@@ -1,4 +1,10 @@
-{ modulesPath, ... }:
+{
+  modulesPath,
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 {
   imports = [
     (modulesPath + "/virtualisation/virtualbox-image.nix")
@@ -6,4 +12,21 @@
   ];
 
   services.xserver.videoDrivers = [ "virtualbox" ];
+
+  # https://github.com/NixOS/nixpkgs/issues/243671#issuecomment-1822983422
+  services.xserver.displayManager.sessionCommands =
+    let
+      inherit (config.boot.kernelPackages) virtualboxGuestAdditions;
+      path = lib.makeBinPath (
+        with pkgs;
+        [
+          gnugrep
+          xorg.xorgserver.out
+          which
+        ]
+      );
+    in
+    ''
+      PATH="${path}:$PATH"  ${lib.getExe' virtualboxGuestAdditions "VBoxClient"} --vmsvga
+    '';
 }
