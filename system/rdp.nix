d67bc67d68c64cfaf765131465e95312291d151b
Mirko Lenz <mirko@mirkolenz.com>
2024-02-23 17:32:44 +0100

    fix: enable polkit extra config

--- system/rdp.nix
+++ system/rdp.nix
@@ -12,23 +12,20 @@ in
 
   environment.systemPackages = lib.singleton gnome-session;
 
-  security.polkit = {
-    enable = false;
-    extraConfig = ''
-      polkit.addRule(function(action, subject) {
-        if (
-          subject.isInGroup("users")
-            && (
-              action.id == "org.freedesktop.login1.reboot" ||
-              action.id == "org.freedesktop.login1.reboot-multiple-sessions" ||
-              action.id == "org.freedesktop.login1.power-off" ||
-              action.id == "org.freedesktop.login1.power-off-multiple-sessions"
-            )
+  security.polkit.extraConfig = ''
+    polkit.addRule(function(action, subject) {
+      if (
+        subject.isInGroup("users")
+          && (
+            action.id == "org.freedesktop.login1.reboot" ||
+            action.id == "org.freedesktop.login1.reboot-multiple-sessions" ||
+            action.id == "org.freedesktop.login1.power-off" ||
+            action.id == "org.freedesktop.login1.power-off-multiple-sessions"
           )
-        {
-          return polkit.Result.YES;
-        }
-      })
-    '';
-  };
+        )
+      {
+        return polkit.Result.YES;
+      }
+    })
+  '';
 }
