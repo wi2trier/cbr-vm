45cdb1b3bae2418bfbf78c5de1ea5f4e6775deea
Mirko Lenz <mirko@mirkolenz.com>
2024-03-07 10:26:32 +0100

    fix: infinite recursion in python module

--- system/python.nix
+++ system/python.nix
@@ -1,16 +1,14 @@
 { pkgs, lib, ... }:
 let
-  python3 =
-    with pkgs;
-    writeShellScriptBin "python3" ''
-      export LD_LIBRARY_PATH=${
-        lib.makeLibraryPath [
-          stdenv.cc.cc
-          zlib
-        ]
-      }
-      exec ${lib.getExe python3} "$@"
-    '';
+  python3 = pkgs.writeShellScriptBin "python3" ''
+    export LD_LIBRARY_PATH=${
+      lib.makeLibraryPath [
+        pkgs.stdenv.cc.cc
+        pkgs.zlib
+      ]
+    }
+    exec ${lib.getExe pkgs.python3} "$@"
+  '';
 in
 {
   system.activationScripts.python.text = ''
